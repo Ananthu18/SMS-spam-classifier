56e2d4b8d5535931984ee944a37eb745677eba3d
Ananthu18 <iamananthu18@gmail.com>
2022-07-06 11:44:11 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[theme]
+base="dark"
+primaryColor="#ffce06"
+backgroundColor="#690303"
+secondaryBackgroundColor="#02555a"
+font = "monospace"
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
