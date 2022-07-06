2e36a3043a80ee00c76335d6f1fa347895c90324
Ananthu18 <iamananthu18@gmail.com>
2022-07-06 12:01:55 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[theme]
-base="dark"
-primaryColor="#ffce06"
-backgroundColor="#690303"
-secondaryBackgroundColor="#02555a"
-font = "monospace"
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
+[theme]
+base= 'dark'
+primaryColor = '#ffce06'
+backgroundColor= '#690303'
+secondaryBackgroundColor= '#02555a'
+font = 'monospace'
+
 \n\
 " > ~/.streamlit/config.toml
